7576920c7427a2ccd56d20178a5c317673f10100
Vu Tran <vutran6853@gmail.com>
2018-02-10 22:18:17 -0600

    clientDashBoard Project

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyACRnJHDvWh6zNN7GBg8xuttQM2FqJOLUU",
+    authDomain: "clientpanelprod-2629b.firebaseapp.com",
+    databaseURL: "https://clientpanelprod-2629b.firebaseio.com",
+    projectId: "clientpanelprod-2629b",
+    storageBucket: "clientpanelprod-2629b.appspot.com",
+    messagingSenderId: "447344302613"
+  }
+};
